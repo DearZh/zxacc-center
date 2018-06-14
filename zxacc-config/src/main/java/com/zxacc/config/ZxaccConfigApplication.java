ffd46c7f3c7dc5d6b2e489aa117ebd88667bbb4f
eko.zhan <eko.z@outlook.com>
2018-06-14 21:13:03 +0800

    zxacc-config

---
+++ zxacc-config/src/main/java/com/zxacc/config/ZxaccConfigApplication.java
@@ -0,0 +1,16 @@
+package com.zxacc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ZxaccConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxaccConfigApplication.class, args);
+	}
+}
