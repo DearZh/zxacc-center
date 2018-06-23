38663be491f5e74a512119d90a0de1e72c765ced
eko.zhan <eko.z@outlook.com>
2018-06-23 15:25:08 +0800

    zxacc-eureka package rename

--- zxacc-eureka/src/main/java/com/zhengxinacc/eureka/ZxaccEurekaApplication.java
+++ zxacc-eureka/src/main/java/com/zhengxinacc/eureka/ZxaccEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.zxacc.eureka;
+package com.zhengxinacc.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zxacc-eureka/src/test/java/com/zhengxinacc/eureka/ZxaccEurekaApplicationTests.java
+++ zxacc-eureka/src/test/java/com/zhengxinacc/eureka/ZxaccEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zxacc.eureka;
+package com.zhengxinacc.eureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
