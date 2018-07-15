4c9ef6520a173b3438b4ea219f04c4b06c5792fd
eko.zhan <eko.z@outlook.com>
2018-07-15 16:56:59 +0800

    zxacc-mgr

---
+++ zxacc-mgr/src/main/java/com/zhengxinacc/mgr/ZxaccMgrApplication.java
@@ -0,0 +1,12 @@
+package com.zhengxinacc.mgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxaccMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxaccMgrApplication.class, args);
+	}
+}
---
+++ zxacc-mgr/src/test/java/com/zhengxinacc/mgr/ZxaccMgrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhengxinacc.mgr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZxaccMgrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
